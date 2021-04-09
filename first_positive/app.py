1b5c1122ef2b6b5a12bf39f96d2baba6d6435dc4
Benilton <bsa.ele@uea.edu.br>
2021-04-09 14:57:36 -0500

    refact: add oparator functions

--- first_positive/app.py
+++ first_positive/app.py
@@ -6,22 +6,23 @@ Por exemplo, na seguinte lista, o output seria o valor 2
 numeros = [1, 3, 10, -2, -14, 20]
 '''
 from typing import List
+from operator import le, gt, ne
 
 
 def app(values: List[int]):
     
-    if sum(values) <= 0:
+    if le(sum(values), 0):
         return 1
    
     if not values:
         return 1
     
     else:
-        filtered_values = list(filter(lambda value: value > 0, sorted(values)))
+        filtered_values = list(filter(lambda value: gt(value, 0), sorted(values)))
         r = range(filtered_values[0], filtered_values[-1])
         
         for R, value in zip(r, filtered_values):
-            if R != value:
+            if ne(R, value):
                 return R
         
         return filtered_values[-1] + 1
